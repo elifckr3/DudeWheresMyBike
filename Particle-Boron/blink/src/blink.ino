e7c847883a42942de8b04dd6ad618f81b9681a1b
Adam von Arnim <vonarnim@usc.edu>
2021-05-01 11:38:00 -0700

    mqtt

--- Particle-Boron/blink/src/blink.ino
+++ Particle-Boron/blink/src/blink.ino
@@ -1,18 +1,49 @@
-/*
- * Project blink
- * Description:
- * Author:
- * Date:
- */
+#include "MQTT.h"
 
-// setup() runs once, when the device is first turned on.
+void callback(char* topic, byte* payload, unsigned int length);
+MQTT client("test.mosquitto.org", 9001, callback);
+
+// configuration settings
+int LOOP_DELAY = 10000; // time is in milliseconds
+bool ON = false;
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
-
+    // LED pin setup
+    client.connect("sparkclient");
+    if (client.isConnected()) {
+        client.subscribe("fromMobile/message");
+    }
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+callback(char* topic, byte& payload, unsigned int length) {
 
+    // modulate state machine from callback
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    Serial.println("payload obtained from server:");
+    for (int i=0;i<length;i++) {
+        if (payload[i]) {
+            ON = true;
+        } else {
+            ON = false;
+        }
+    }
+    // Some stuff to make with the payload obtained
+        //
+   //
+}
+
+void loop() {
+    // We are sending the temperature and humidity level in one publish
+    // sample latitude/longitude/acceleration
+    if (client.isConnected()) {
+
+        lati = 34.99;
+        longi = -118.99;
+        acc = 3;
+        
+        Particle.publish("update", String::format("{\"latitude\":%d,\"longitude\":%d,\"acceleration\":%d}", lati, longi, acc), PRIVATE);
+        delay(LOOP_DELAY);
+    }
 }
